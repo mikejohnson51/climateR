a759e03c1c8476a259f3d450419f9920d30a2a49
mikejohnson51 <mike.johnson@geog.ucsb.edu>
2018-11-29 21:16:31 -0800

    fix CHIRPS

--- R/getCHIRPS.R
+++ R/getCHIRPS.R
@@ -2,6 +2,8 @@ getCHIRPS = function(AOI, startDate, endDate = NULL  ){
 
   d = define.dates  (startDate, endDate)
 
+  bb = AOI::bbox_st(AOI)
+
   dates = paste0("%28", as.numeric(format(d$date, "%d")), "%20", month.abb[as.numeric(format(d$date, "%m"))], "%20", format(d$date, "%Y"))
 
   urls = paste0('https://iridl.ldeo.columbia.edu/SOURCES/.UCSB/.CHIRPS/.v2p0/.daily-improved/.global/.0p05/.prcp/',
